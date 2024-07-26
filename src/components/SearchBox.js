cd0c1272a8d9bc610f34f6436ab330b6738aa018
Andrew Tedjapratama <122066488+Fozzzzy@users.noreply.github.com>
2024-07-26 20:03:08 +0700

    feat: added heading and search box

---
+++ src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = (props) => {
+    return (
+        <div className="col col-sm-4">
+            <input className="form-control" placeholder="Search movies... "></input>
+        </div>
+    )
+}
+
+export default searchBox;
