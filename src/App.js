4b44da5c46a19b2d9213bf0a9690b184b9060a35
Andrew Tedjapratama <122066488+Fozzzzy@users.noreply.github.com>
2024-07-26 21:56:14 +0700

    feat: write favourites to local storage

--- src/App.js
+++ src/App.js
@@ -3,35 +3,81 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import MovieList from './components/MovieList'; 
 import MovieListHeading from './components/MovieListHeading'; 
-
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-
+  const [favourites, setFavourites] = useState([]);
   const [searchValue, setSearchValue] = useState('');
   
-  const getMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=avengers&apikey=7c963a90";
+  // Get movies from OMDB API
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=7c963a90`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
 
-    console.log(responseJson);
-    setMovies(responseJson.Search);
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
   }
 
+  // Get movie request everytime search value changes
   useEffect (() => {
-    getMovieRequest();
-  }, [])
+    getMovieRequest(searchValue);
+  }, [searchValue])
+
+  // Write to local storage everytime movie favourites is changed
+  useEffect (() => {
+    const movieFavourites = JSON.parse(localStorage.getItem('movie-list-favourites'))
+    setFavourites(movieFavourites);
+  }, []);
+
+  // Add to favourites
+  const addFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
+
+  // Remove from favourites
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter((favourite) => favourite.imdbID !== movie.imdbID);
+    setFavourites(newFavouriteList);
+  }
+
+  // Save to localStorage
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('movie-list-favourites', JSON.stringify(items));
+  }
 
 
+
+  // Render
   return (
     <div className='container-fluid'>
-      <div className='row'>
-        <MovieListHeading heading="Movies"/>
+      <div className='rows d-flex flex-column align-items-center my-4'>
+        <MovieListHeading heading="Movies Search"/>
+        <SearchBox 
+          searchValue={searchValue} 
+          setSearchValue={setSearchValue}/>
       </div>
-      <div className='d-flex rows'>
-        <MovieList movies = {movies} />
+      <div className='rows d-flex'>
+        <MovieList 
+          movies = {movies} 
+          handleFavouritesClick={addFavouriteMovie} 
+          FavouriteComponent={AddFavourites}/>
+      </div>
+      <div className='rows d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading="Favourites"/>
+      </div>
+      <div className='rows d-flex'>
+        <MovieList 
+          movies = {favourites} 
+          handleFavouritesClick={removeFavouriteMovie} 
+          FavouriteComponent={RemoveFavourites}/>
       </div>
     </div>
   );
